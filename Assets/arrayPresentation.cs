bbd48f6af3d58e49d81ea52ae5ca88c682d8a88d
Paul B <paul.belfrage@gmail.com>
2020-03-31 13:26:14 +0200

    Looping Through Data Presentation
    Looping Through Data Presentation

---
+++ Assets/arrayPresentation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string name;
+    public string description;
+}
+
+public class arrayPresentation : MonoBehaviour
+{
+    public Item[] myItems;
+    void Start()
+    {
+        foreach (var item in myItems)
+        {
+            print(item.name);
+            if (item.itemID == 0)
+            {
+                print("You have it");
+            }
+            else
+            {
+                print("You do not have this item");
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int randomID = Random.Range(0, myItems.Length);
+
+            print("itemID: " + myItems[randomID].itemID + ". Name: " + myItems[randomID].name + ". Description: " + myItems[randomID].description);
+        }
+    }
+}
