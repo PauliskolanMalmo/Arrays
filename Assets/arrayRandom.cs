8cc8a9a3d74df4db512b4340ad42d523238e4534
Paul B <paul.belfrage@gmail.com>
2020-03-31 10:40:56 +0200

    Challenge Review Randomly Choose One
    Challenge Review Randomly Choose One

---
+++ Assets/arrayRandom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrayRandom : MonoBehaviour
+{
+    public string[] names = { "A", "B", "C", "D", "E" };
+    public int[] ages = { 1, 2, 3, 4, 5 };
+    public string[] cars = { "a", "b", "c", "d", "e" };
+
+    public int randomID;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            randomID = Random.Range(0, names.Length);
+            print(randomID);
+            print("Name: " + name[randomID] + "Age: " + ages[randomID] + "  Car:  " + cars[randomID]);
+        }
+    }
+}
